e335b543365ef377823305b073841a5f6932bd1f
LeeHao <1838249551@qq.com>
2024-01-11 11:10:51 +0800

    style: format code

--- src/kv.rs
+++ src/kv.rs
@@ -1,21 +1,20 @@
-use std::{collections::HashMap, clone};
+use std::collections::HashMap;
+
 
 pub struct KvStore {
-  mp: HashMap<String, String>,
+    mp: HashMap<String, String>,
 }
 impl KvStore {
-  pub fn new() -> KvStore {
-    KvStore {
-      mp: HashMap::new(),
+    pub fn new() -> KvStore {
+        KvStore { mp: HashMap::new() }
     }
-  }
-  pub fn set(&mut self, key: String, val: String) {
-    self.mp.insert(key, val);
-  }
-  pub fn get(&mut self, key: String) -> Option<String> {
-    self.mp.get(&key).cloned()
-  }
-  pub fn remove(&mut self, key: String) {
-    self.mp.remove(&key);
-  }
+    pub fn set(&mut self, key: String, val: String) {
+        self.mp.insert(key, val);
+    }
+    pub fn get(&mut self, key: String) -> Option<String> {
+        self.mp.get(&key).cloned()
+    }
+    pub fn remove(&mut self, key: String) {
+        self.mp.remove(&key);
+    }
 }
